2eb176271486d83c99e061b4e2211367b286273d
이상경 <sknglee22@naver.com>
2020-09-24 21:16:33 +0900

    feat:App() 리턴 항목값 추가

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>hi</h1>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        h1 > hi < /h1> <
+        p > 👍깃 스페셜 세션 너무 좋아요~~👍 < /p> <
+        /div>
+    );
 }
 
 export default App;
